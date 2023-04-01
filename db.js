4a72e72fb71a62980feb7f1a912feef5d070618c
Marcel Hughes <marcellondon@icloud.com>
2023-04-01 12:15:57 -0500

    creates server files, structures db

---
+++ db.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+        title: `The Night Dad Went to Jail: What to Expect When Someone You Love Goes to Jail (Life's Challenges)`,
+        author: `Melissa Higgins`,
+        publish_date: `2011`,
+        description: `This picture book helps children understand and cope with the difficult emotions and confusion they may experience when a loved one is incarcerated. Through the eyes of a young rabbit named Sketch, the story explores the various stages of the process, from the arrest to visitations and release, offering guidance and support for children dealing with this challenging situation.`,
+        genre: [`Children’s Literature`, `Social Issues`, `Self-Help`],
+    },
+    {
+        title: `Fifty Shades of Grey`,
+        author: `E. L. James`,
+        publish_date: `2011`,
+        description: `An erotic romance novel that follows the relationship between college graduate Anastasia Steele and young business magnate Christian Grey, as they explore themes of love, dominance, and submission`,
+        genre: [`Erotic Romance`, `Contemporary Romance`],
+    },
+    {
+        title: `How to Teach Quantum Physics to Your Dog`,
+        author: `Chad Orzel`,
+        publish_date: `December 12, 2009`,
+        description: `A unique and entertaining introduction to quantum physics, using conversations with the author’s dog to explain complex scientific concepts in a way that is engaging and accessible`,
+        genre: [`Popular Science`, `Physics`, `Humor`,]
+    },
+    {
+        title: `Always Ask a Man: The Key to Femininity`,
+        author: `Arlene Dahl`,
+        publish_date: `1965`,
+        description: `A tongue -in -cheek guide for women offering advice on fashion, beauty, and relationships, written by the well - known fashion designer and television personality Arlene Dahl.`,
+        genre: [`Self - Help`, `Humor`, `Fashion`]
+    },
+    {
+        title: `I Gave You My Heart, But You Sold It Online`,
+        author: `Dixie Cash`,
+        publish_date: `2006`,
+        description: `A romantic comedy about a woman who starts a new life and opens an Internet security business after discovering her husband’s infidelity, only to find herself caught up in a dangerous cybercrime investigation.`,
+        genre: [`Romance`, `Mystery`, `Humor`],
+},
+    {
+          title: `Why Isn't God Giving Cash Prizes?`,
+          author: `Peter Cook`,
+          publish_date: `1991`,
+          description: `A humorous collection of essays from comedian and writer Peter Cook that explores various aspects of life, politics, and religion, offering readers an insightful and entertaining perspective on the world.`,
+          genre: [`Humor`, `Essays`, `Satire`],
+},
+    {
+          title: `Conspiracy of Fools`,
+          author: `Kurt Eichenwald`,
+          publish_date: `2005`,
+          description: `A non - fiction narrative detailing the rise and fall of Enron, one of the largest corporate scandals in American history, and the various people and decisions involved in its collapse.`,
+          genre: [`Non - Fiction`, `Business`, `True Crime`,],
+},
+    {
+          title: `The Big Short`,
+          author: `Michael Lewis`,
+          publish_date: `2010`,
+          description: `A non - fiction book that tells the story of the 2008 financial crisis, focusing on the individuals who foresaw the subprime mortgage bubble and made huge profits by betting against the housing market.`,
+          genre: [`Non - Fiction`, `Economics`, `History`,],
+},
+]
